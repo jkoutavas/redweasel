5cd2e16661a8d3b37deba35f13e3965bfe5e65c8
Jay Koutavas <jay@heynow.com>
2017-01-31 17:09:23 -0500

    Licensed now as GNU GPL v3

--- common/RedWeasel/Source/AppProperties.h
+++ common/RedWeasel/Source/AppProperties.h
@@ -1,3 +1,32 @@
+/********************************************************************************
+ *   _    _                                                                     *
+ *  | |  | |                                             Copyright 2017         *
+ *  | |__| |   ___   _   _   _ __     ___   __      __   Heynow Software, LLC   *
+ *  |  __  |  / _ \ | | | | | '_ \   / _ \  \ \ /\ / /                          *
+ *  | |  | | |  __/ | |_| | | | | | | (_) |  \ V  V /                           *
+ *  |_|  |_|  \___|  \__, | |_| |_|  \___/    \_/\_/                            *
+ *                    __/ |                                                     *
+ *                   |___/  S  O  F  T  W  A  R  E                              *
+ *                                                                              *
+ *  author: Jay Koutavas                                                        *
+ *  email: jay@heynow.com                                                       *
+ *                                                                              *
+ *  This file is part of the "Red Weasel" application.  This application is     *
+ *  free software; you can redistribute it and/or modify it under the terms     *
+ *  of the GNU General Public License as published by the Free Software         *
+ *  Foundation; either version 3, or (at your option) any later version.        *
+ *                                                                              *
+ *  This application is distributed in the hope that it will be useful, but     *
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  *
+ *  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    *
+ *  for more details.                                                           *
+ *                                                                              *
+ *  You should have received a copy of the GNU General Public License along     *
+ *  with this application's source code; see the file COPYING. If not, see      *
+ *  <http://www.gnu.org/licenses/>.                                             *
+ *                                                                              *
+ ********************************************************************************/
+ 
 #ifndef APPPROPERTIES_H_INCLUDED
 #define APPPROPERTIES_H_INCLUDED
 
